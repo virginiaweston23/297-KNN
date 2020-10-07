6d36114c9230ae9d3866fb900f3c5ffd55651b54
jind2021 <31631979+0oTinaKimo0@users.noreply.github.com>
2020-10-07 09:03:39 -0400

    Add files via upload

--- KNN.py
+++ KNN.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score
+import sklearn.preprocessing as pp
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+import time
+
 
 # Loading the prostate cancer dataset from the csv file using pandas
 data = pd.read_csv('prostate-cancer-prediction.csv', header=0)
@@ -23,15 +25,22 @@ print('Class labels:', np.unique(y))
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
 
 # Standardizing the features:
-sc = StandardScaler()
+sc = pp.PowerTransformer()
+# sc = pp.StandardScaler()
+# sc = pp.MaxAbsScaler()
+# sc = pp.RobustScaler()
 sc.fit(X_train)
 X_train_std = sc.transform(X_train)
 X_test_std = sc.transform(X_test)
 
 # K-nearest neighbors
-knn = KNeighborsClassifier(n_neighbors=5,
-                           p=2,
-                           metric='minkowski')
-knn.fit(X_train_std, y_train)
-y_pred = knn.predict(X_test_std)
-print("KNN Accuracy: %3f" % accuracy_score(y_test, y_pred))
+# Best result given by n = 25, metric = 'manhattan'
+knn_n = [5, 10, 15, 20, 25, 30, 35]
+for i in knn_n:
+    t_b = time.perf_counter()
+    knn = KNeighborsClassifier(n_neighbors=i, metric='manhattan')
+    knn.fit(X_train_std, y_train)
+    y_pred = knn.predict(X_test_std)
+    t_e = time.perf_counter()
+    print("KNN Accuracy: {} when n = {}".format(accuracy_score(y_test, y_pred), i))
+    print((t_e - t_b)*1000)
