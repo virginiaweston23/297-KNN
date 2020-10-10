9bc82c4ec097606e5d1d0fd9cdf8ea52e9bf639b
jind2021 <31631979+0oTinaKimo0@users.noreply.github.com>
2020-10-10 11:26:21 -0400

    Add files via upload

--- KNN.py
+++ KNN.py
@@ -13,6 +13,7 @@ import time
 data = pd.read_csv('prostate-cancer-prediction.csv', header=0)
 data.columns = ['ID', 'OUT', 'RAD', 'TEX', 'PERI', 'AREA', 'SMOO', 'COMP', 'SYMM', 'DIM']
 features = ['RAD', 'TEX', 'PERI', 'AREA', 'SMOO', 'COMP', 'SYMM', 'DIM']
+
 X = data[features]
 y = data['OUT']
 print('Class labels:', np.unique(y))
@@ -25,29 +26,35 @@ y = y.astype('int').T
 print('Class labels:', np.unique(y))
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
 
-# Standardizing the features:
-sc = pp.PowerTransformer()
-# sc = pp.StandardScaler()
-# sc = pp.MaxAbsScaler()
-# sc = pp.RobustScaler()
-sc.fit(X_train)
-X_train_std = sc.transform(X_train)
-X_test_std = sc.transform(X_test)
+# Standardize the features:
+scaler = pp.PowerTransformer()
+# scaler = pp.StandardScaler()
+# scaler = pp.MaxAbsScaler()
+# scaler = pp.RobustScaler()
+scaler.fit(X_train)
+X_train_std = scaler.transform(X_train)
+X_test_std = scaler.transform(X_test)
 
 # K-nearest neighbors
-# Best result given by n = 25, metric = 'manhattan'
-knn_n = [5, 10, 15, 20, 25, 30, 35]
-for i in knn_n:
+knn_k = [5, 10, 15, 20, 25, 30, 35]
+res_pred = []
+res_times = []
+for i in knn_k:
     t_b = time.perf_counter()
     knn = KNeighborsClassifier(n_neighbors=i, metric='manhattan', weights='distance')
     knn.fit(X_train_std, y_train)
     y_pred = knn.predict(X_test_std)
+    res_pred.append(accuracy_score(y_test, y_pred))
     t_e = time.perf_counter()
-    print("KNN Accuracy: {} when n = {}".format(accuracy_score(y_test, y_pred), i))
-    print((t_e - t_b)*1000)
+    res_times.append((t_e - t_b)*1000)
+plt.plot(knn_k, res_pred, 'r--', knn_k, res_times, 'b--')
+plt.xlabel('K')
+plt.ylabel('Accuracy (red) and Time (blue)')
+plt.show()
 
-knn_optimal = KNeighborsClassifier(n_neighbors=30, metric='manhattan', weights='distance')
+# Best result given by n = 15, metric = 'manhattan', weights = 'distance'
+knn_optimal = KNeighborsClassifier(n_neighbors=15, metric='manhattan', weights='distance')
 knn_optimal.fit(X_train_std, y_train)
 y_optimal_pred = knn_optimal.predict(X_test_std)
 print("KNN Accuracy: {}".format(accuracy_score(y_test, y_optimal_pred)))
-print("Other Metrics: {}".format(classification_report(y_test, y_optimal_pred)))
+print("Metrics: {}".format(classification_report(y_test, y_optimal_pred)))
